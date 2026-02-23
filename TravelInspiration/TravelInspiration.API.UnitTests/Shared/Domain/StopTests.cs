aef92686e6c8b3b8a6beb4e2f6c672282770758f
Mohamadreza <farahbakhsh20mohamadreza30@gmail.com>
2026-02-24 00:17:31 +0330

    Shared context between tests(Constructor and dispose)

--- TravelInspiration/TravelInspiration.API.UnitTests/Shared/Domain/StopTests.cs
+++ TravelInspiration/TravelInspiration.API.UnitTests/Shared/Domain/StopTests.cs
@@ -4,34 +4,72 @@ using TravelInspiration.API.Shared.Domain.Events;
 
 namespace TravelInspiration.API.UnitTests.Shared.Domain;
 
-public class StopTests
+public class StopTests : IDisposable
 {
+    private readonly Stop _stopEntity;
+    private readonly CreateStopCommand _createStopCommand;
+
+    public StopTests()
+    {
+        _stopEntity = new Stop("StopForTesting");
+        _createStopCommand = new CreateStopCommand(42, "A Name", null);
+    }
+
     [Fact]
     public void WhenExecutingHandleCreateCommand_WithItineraryId_StopItineraryIdMustMatch()
     {
         //ARRANGE
-        var stopEntity = new Stop("StopForTesting");
-        var createStopCommand = new CreateStopCommand(42, "A Name", null);
 
         //ACT
-        stopEntity.HandleCreateCommand(createStopCommand);
+        _stopEntity.HandleCreateCommand(_createStopCommand);
 
         //ASSERT
-        Assert.Equal(createStopCommand.ItineraryId, stopEntity.ItineraryId);
+        Assert.Equal(_createStopCommand.ItineraryId, _stopEntity.ItineraryId);
     }
 
     [Fact]
     public void WhenExecutingCreateCommand_WithValidInput_OneStopCreatedEventMustBeAdded()
     {
         //ARRANGE
-        var stopEntity = new Stop("StopForTesting");
-        var createStopCommand = new CreateStopCommand(42, "A Name", null);
 
         //ACT
-        stopEntity.HandleCreateCommand(createStopCommand);
+        _stopEntity.HandleCreateCommand(_createStopCommand);
 
         //ASSERT
-        Assert.Single(stopEntity.DomainEvents);
-        Assert.IsType<StopCreatedEvent>(stopEntity.DomainEvents[0]);
+        Assert.Single(_stopEntity.DomainEvents);
+        Assert.IsType<StopCreatedEvent>(_stopEntity.DomainEvents[0]);
     }
+
+    public void Dispose()
+    {
+    }
+
+    //[Fact]
+    //public void WhenExecutingHandleCreateCommand_WithItineraryId_StopItineraryIdMustMatch()
+    //{
+    //    //ARRANGE
+    //    var stopEntity = new Stop("StopForTesting");
+    //    var createStopCommand = new CreateStopCommand(42, "A Name", null);
+
+    //    //ACT
+    //    stopEntity.HandleCreateCommand(createStopCommand);
+
+    //    //ASSERT
+    //    Assert.Equal(createStopCommand.ItineraryId, stopEntity.ItineraryId);
+    //}
+
+    //[Fact]
+    //public void WhenExecutingCreateCommand_WithValidInput_OneStopCreatedEventMustBeAdded()
+    //{
+    //    //ARRANGE
+    //    var stopEntity = new Stop("StopForTesting");
+    //    var createStopCommand = new CreateStopCommand(42, "A Name", null);
+
+    //    //ACT
+    //    stopEntity.HandleCreateCommand(createStopCommand);
+
+    //    //ASSERT
+    //    Assert.Single(stopEntity.DomainEvents);
+    //    Assert.IsType<StopCreatedEvent>(stopEntity.DomainEvents[0]);
+    //}
 }
