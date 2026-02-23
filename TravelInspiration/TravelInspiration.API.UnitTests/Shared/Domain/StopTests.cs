4765e2553295c29852a87042a8ba7fe2c57a7bc0
Mohamadreza <farahbakhsh20mohamadreza30@gmail.com>
2026-02-23 23:54:18 +0330

    Unit Test with xUnit

---
+++ TravelInspiration/TravelInspiration.API.UnitTests/Shared/Domain/StopTests.cs
@@ -0,0 +1,37 @@
+﻿using TravelInspiration.API.Features.Stops;
+using TravelInspiration.API.Shared.Domain.Entities;
+using TravelInspiration.API.Shared.Domain.Events;
+
+namespace TravelInspiration.API.UnitTests.Shared.Domain;
+
+public class StopTests
+{
+    [Fact]
+    public void WhenExecutingHandleCreateCommand_WithItineraryId_StopItineraryIdMustMatch()
+    {
+        //ARRANGE
+        var stopEntity = new Stop("StopForTesting");
+        var createStopCommand = new CreateStopCommand(42, "A Name", null);
+
+        //ACT
+        stopEntity.HandleCreateCommand(createStopCommand);
+
+        //ASSERT
+        Assert.Equal(createStopCommand.ItineraryId, stopEntity.ItineraryId);
+    }
+
+    [Fact]
+    public void WhenExecutingCreateCommand_WithValidInput_OneStopCreatedEventMustBeAdded()
+    {
+        //ARRANGE
+        var stopEntity = new Stop("StopForTesting");
+        var createStopCommand = new CreateStopCommand(42, "A Name", null);
+
+        //ACT
+        stopEntity.HandleCreateCommand(createStopCommand);
+
+        //ASSERT
+        Assert.Single(stopEntity.DomainEvents);
+        Assert.IsType<StopCreatedEvent>(stopEntity.DomainEvents[0]);
+    }
+}
